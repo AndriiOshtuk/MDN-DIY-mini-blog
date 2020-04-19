2f8b391c2910594fcc6ad380e97d5029f1232615
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-04-19 13:29:58 +0300

    Added blogger list and detail views.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from blog.models import Blogger
 
 # Create your views here.
+class BloggerListView(generic.ListView):
+    model = Blogger
+
+
+class BloggerDetailView(generic.DetailView):
+    model = Blogger
+
+
+def index(request):
+    pass
