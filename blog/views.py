55653fbcfe4a8508fd652d4595c0839253e51515
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-04-19 13:57:03 +0300

    Added blog list view and template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from blog.models import Blogger
+from blog.models import Blogger, Post
 
 # Create your views here.
 class BloggerListView(generic.ListView):
@@ -9,6 +9,14 @@ class BloggerListView(generic.ListView):
 
 class BloggerDetailView(generic.DetailView):
     model = Blogger
+
+
+class PostListView(generic.ListView):
+    model = Post
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
 
 
 def index(request):
