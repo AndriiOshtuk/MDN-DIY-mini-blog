3eca76b3831c5cafea223006361140777527dcd8
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-04-19 13:29:30 +0300

    Redirect both /and blog/ to blog module urls.py

--- diyblog/urls.py
+++ diyblog/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls'))
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='blog/', permanent=True)),
 ]
