0ed33bec929ba498a177dacbb00be9634632cf3a
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-04-19 16:14:45 +0300

    Added allowed hosts

--- diyblog/settings.py
+++ diyblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '710-9_#7)v3b&n)bur72&=#z^gqrji
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'mdn-diy-mini-blog.herokuapp.com']
 
 
 # Application definition
