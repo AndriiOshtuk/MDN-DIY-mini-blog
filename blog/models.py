e8978ec6e5fbfc886f4a6428931152ff5bbcec03
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-04-19 13:56:21 +0300

    Renamed post-details to blog-details

--- blog/models.py
+++ blog/models.py
@@ -29,11 +29,11 @@ class Post(models.Model):
     # comments = 
 
     class Meta:
-        ordering = ['post_date']
+        ordering = ['-post_date']
  
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post-detail', args=[str(self.id)])
+        return reverse('blog-detail', args=[str(self.id)])
 
--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,11 @@ urlpatterns = [
     path('',  views.index, name='index'),
     path('bloggers/', views.BloggerListView.as_view(), name='bloggers'),
     path('blogger/<int:pk>', views.BloggerDetailView.as_view(), name='blogger-detail'),
+    path('blogs/', views.PostListView.as_view(), name='blogs'),
+    path('blog/<int:pk>', views.PostDetailView.as_view(), name='blog-detail'),
 
-    path('blogs/', views.index, name='blogs'),
+    
     path('logout/', views.index, name='logout'),
     path('login/', views.index, name='login'),
-    path('post/<int:pk>', views.index, name='post-detail'),
+    
 ]
