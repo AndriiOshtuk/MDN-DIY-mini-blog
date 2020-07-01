16eccb5ed67d986e7d24cb3f2bf7221a2f66c292
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-07-01 17:12:40 +0300

    Added doc strings for forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,16 @@
 from django.forms import ModelForm
-
 from blog.models import Comment
 
+
 class AddCommentModelForm(ModelForm):
+    """
+    Form to create instance of :model:'blog.Comment'
+    
+    **Template:**
+    :template:'blog/comment_form.html'
+    
+    """
     class Meta:
         model = Comment
         fields = ['text']
+ 
