0096bbf898f86807e2366ee6900aa6c112fb371f
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-07-04 12:45:04 +0300

    Renamed testcases

--- blog/tests/test_forms.py
+++ blog/tests/test_forms.py
@@ -8,6 +8,6 @@ class AddCommentModelFormTest(SimpleTestCase):
         form = AddCommentModelForm()
         self.assertTrue(form.fields['text'].label == None or form.fields['text'].label == 'Description')
 
-    def test_renew_form_date_field_help_text(self):
+    def test_text_field_help_text(self):
         form = AddCommentModelForm()
         self.assertEqual(form.fields['text'].help_text, 'Enter comment comment about blog here.')
