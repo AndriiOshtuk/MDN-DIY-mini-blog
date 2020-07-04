cba40f15419d67377ac43553611c78e724c564c8
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-07-04 08:50:00 +0300

    Added forms tests
    Implemented AddCommentModelFormTest

---
+++ blog/tests/test_forms.py
@@ -0,0 +1,13 @@
+from django.test import SimpleTestCase
+
+from blog.forms import AddCommentModelForm
+
+
+class AddCommentModelFormTest(SimpleTestCase):
+    def test_renew_form_date_field_label(self):
+        form = AddCommentModelForm()
+        self.assertTrue(form.fields['text'].label == None or form.fields['text'].label == 'Description')
+
+    def test_renew_form_date_field_help_text(self):
+        form = AddCommentModelForm()
+        self.assertEqual(form.fields['text'].help_text, 'Enter comment comment about blog here.')
