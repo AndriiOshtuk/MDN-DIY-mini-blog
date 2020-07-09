7ad51d55d05e2eef0a3898df3b84d2c70f70b799
Andriy Oshtuk <andrii.oshtuk@gmail.com>
2020-07-09 21:56:49 +0300

    Fixed flake8 warnings

--- blog/forms.py
+++ blog/forms.py
@@ -5,12 +5,11 @@ from blog.models import Comment
 class AddCommentModelForm(ModelForm):
     """
     Form to create instance of :model:'blog.Comment'
-    
+
     **Template:**
     :template:'blog/comment_form.html'
-    
+
     """
     class Meta:
         model = Comment
         fields = ['text']
- 
